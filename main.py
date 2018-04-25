5be15b4e65b5687c48780f14805a0078ab8e92a5
NCA\77003 <raymondsryang@gmail.com>
2018-04-26 07:31:39 +0800

    Template ver1

---
+++ main.py
@@ -0,0 +1,12 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def main():
+    dosomthing = 0
+
+
+if __name__ == "__main__":
+    main()
