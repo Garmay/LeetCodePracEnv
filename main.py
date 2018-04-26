dd59b6b2a5109d75c8935bcae8a4dfcde6afea12
garmay <garmay.000@gmail.com>
2018-04-26 16:16:16 +0800

    Change constructor's aruments

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, x=None):
+        self.val = x if x else 0
         self.left = None
         self.right = None
 
 
 def main():
-    dosomthing = 0
-    print("wow")
+
+
+
+def list2tree(m_list):
+    root = None
+    if m_list:
+        root = TreeNode(m_list.pop(0))
+        queue = [root]
+    while m_list:
+        node = queue.pop(0)
+        if not m_list:
+            break
+        node.left = TreeNode(m_list.pop(0))
+        queue.append(node.left)
+
+        if not m_list:
+            break
+        node.right = TreeNode(m_list.pop(0))
+        queue.append(node.right)
+    return root
+
+
+def travel_tree(root):
+    if root is None:
+        return
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        print(node.val)
+
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
 
 
 if __name__ == "__main__":
